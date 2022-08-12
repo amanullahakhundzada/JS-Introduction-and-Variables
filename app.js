03bdd4ad5180d10023826444a9f8b957c9cddfdb
amanullahakhundzada <amanullahkhan9631@gmail.com>
2022-08-11 17:17:52 -0700

    javescript exercise

---
+++ app.js
@@ -0,0 +1,25 @@
+let lastName ;
+var age ;
+
+
+const firstName = "Amanullah";
+ lastName = "Akhundzada";
+ age = 25;
+
+let language = "JavaScript";
+let creatYear=1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale =false;
+let salePercetage = 15;
+let stock =0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let auther = "patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead= true;
+console.log(firstName,lastName,age,language,creatYear,isCaseSensitive,price,isOnSale,salePercetage,stock,inStock,selectedSize,title,auther,pageCount,bookmark,hasRead);
